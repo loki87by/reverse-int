8f9027056a3f2bd6b113fd1a719cb0e22fecd3d0
Alexey Akulich <loki1987by@yandex.ru>
2021-02-28 00:51:25 +0300

    feat: write needed function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let res = 0;
+  let num;
+  if(n < 0){num = n * -1}
+  else{num = n}
+  while(num > 0){
+    res = res * 10 + num % 10
+    num = Math.floor(num / 10)
+  }
+  return res
 }
